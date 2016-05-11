4537d3a1b4bdb462feb12913b0a3b80ef3562be9
Jon.Coello <Jon.Coello@WLK5CB31214W6>
2016-05-11 21:31:12 +0100

    Add project files.

---
+++ EchoBotQuickStart/App_Start/WebApiConfig.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Practices.Unity;
+using Microsoft.Skype.Bots;
+using Microsoft.Skype.Bots.Events.Messaging;
+using Microsoft.Skype.Bots.Interfaces;
+using Unity.WebApi;
+
+namespace EchoBotQuickStart
+{
+    public static class WebApiConfig
+    {
+        
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            var container = new UnityContainer();
+
+            RegisterTypes(container);
+
+            config.DependencyResolver = new UnityDependencyResolver(container);
+        }
+
+        private static void RegisterTypes(UnityContainer container)
+        {
+            MessagingBotServiceSettings settings = MessagingBotServiceSettings.LoadFromCloudConfiguration();
+            IMessagingBotService botService = new BotService(settings);
+
+            container
+                .RegisterInstance(botService, new ContainerControlledLifetimeManager());
+
+            botService.OnPersonalChatMessageReceivedAsync += BotService_OnPersonalChatMessageReceivedAsync;
+                //async receivedEvent =>            
+                //    await receivedEvent.Reply("Well " + receivedEvent.Content + " " + i, true).ConfigureAwait(false);
+
+            botService.OnContactAddedAsync +=
+                async contactAdded =>
+                    await contactAdded.Reply($"Hello {contactAdded.FromDisplayName}!").ConfigureAwait(false);
+        }
+
+        private async static Task BotService_OnPersonalChatMessageReceivedAsync(MessageReceivedEvent receivedEvent) {
+
+            string answer = "I do not understand the question";
+
+            string message = receivedEvent.Content;
+            if (message == "Hello") {
+                answer = "Hello " + receivedEvent.From;
+            }
+
+            await receivedEvent.Reply(answer, true).ConfigureAwait(false);
+        }
+    }
+}
---
+++ EchoBotQuickStart/Controllers/EchoBotController.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Microsoft.Skype.Bots;
+using Microsoft.Skype.Bots.Filters;
+using Microsoft.Skype.Bots.Interfaces;
+
+namespace EchoBotQuickStart.Controllers
+{
+    public class EchoBotController : BotController
+    {
+        #region Static Fields
+
+        private static readonly MtlsAuthenticationFromHeader MtlsAuth = new MtlsAuthenticationFromHeader();
+
+        #endregion
+
+        #region Constructors and Destructors
+
+        public EchoBotController(
+            IMessagingBotService messageProcessor) : base(messageProcessor, "EchoBot")
+        {
+        }
+
+        #endregion
+
+        #region Public Methods and Operators
+
+        [Route("v1/echo")]
+        public override Task<HttpResponseMessage> ProcessMessagingEventAsync()
+        {
+            /* Uncomment following lines to enable https verification for Azure.
+
+            if (!MtlsAuth.ValidateClientCertificate(Request.RequestUri.ToString(), Request.Headers, GetClientIp()))
+            {
+                return Task.FromResult(Request.CreateResponse(HttpStatusCode.Forbidden));
+            }
+            */
+
+            return base.ProcessMessagingEventAsync();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private string GetClientIp()
+        {
+            const string MsHttpContextName = "MS_HttpContext";
+
+            if (Request.Properties.ContainsKey(MsHttpContextName))
+            {
+                return ((HttpContextWrapper) Request.Properties[MsHttpContextName]).Request.UserHostAddress;
+            }
+
+            return null;
+        }
+
+        #endregion
+    }
+}
